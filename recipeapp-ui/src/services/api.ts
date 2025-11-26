4ea048d5d9e008e44bcef02f2abac89f0f25b016
lehoucqgj <gertjan.lehoucq@gmail.com>
2025-11-26 21:23:20 +0100

    helper get made in api service

--- recipeapp-ui/src/services/api.ts
+++ recipeapp-ui/src/services/api.ts
@@ -1,3 +1,14 @@
-import type { Recipe, ShoppingListItem } from '../types';
+import type { Recipe, /*ShoppingListItem*/ } from '../types';
 const API_URL = 'http://localhost:3000';
 
+async function get<Recipe>(endpoint:string): Promise<Recipe> {
+    const response = await fetch(`${API_URL}${endpoint}`);
+    if (!response.ok){
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+}
+
+export const recipeApi = {
+    getAllRecipes: () => get<Recipe[]>('/films')
+}
