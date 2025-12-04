cf861be89bba5979d32e0f4b719061f209a9ff95
lehoucqgj <gertjan.lehoucq@gmail.com>
2025-12-04 21:57:16 +0100

    fixxed the api.ts. array comes out in log, but delayed. prolly something usefx-y. looking into that

--- recipeapp-ui/src/services/api.ts
+++ recipeapp-ui/src/services/api.ts
@@ -1,15 +1,7 @@
 import type { Recipe, Ingredient } from '../types';
 const API_URL = 'http://localhost:3000';
 
-async function getAllRecipes<Recipe>(endpoint:string): Promise<Recipe> {
-    const response = await fetch(`${API_URL}${endpoint}`);
-    if (!response.ok){
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return response.json();
-}
-
-async function getAllRecipeIngredients<Ingredient>(endpoint:string): Promise<Ingredient>{
+async function get<T>(endpoint:string): Promise<T> {
     const response = await fetch(`${API_URL}${endpoint}`);
     if (!response.ok){
         throw new Error(`HTTP error! status: ${response.status}`);
@@ -18,6 +10,6 @@ async function getAllRecipeIngredients<Ingredient>(endpoint:string): Promise<Ing
 }
 
 export const recipeApi = {
-    getAllRecipes: () => getAllRecipes<Recipe[]>('/recipes'),
-    getAllRecipeIngredients: (id: number) => getAllRecipeIngredients<Ingredient[]>(`/recipes/${id}/ingredients`)
+    getAllRecipes: () => get<Recipe[]>('/recipes'),
+    getAllRecipeIngredients: (id: number) => get<Ingredient[]>(`/recipes/${id}/ingredients`)
 };
