0a22c60b05306d2891b5fec739ea5eba0bc84882
lehoucqgj <gertjan.lehoucq@gmail.com>
2025-12-15 21:09:40 +0100

    form for recipe should be ready (needs some styling). api helpers not yet made

--- recipeapp-ui/src/App.tsx
+++ recipeapp-ui/src/App.tsx
@@ -1,13 +1,21 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
 
 import { NavBar } from './components/NavBar'
 import { Home } from './components/Home'
 import { RecipeList } from './components/RecipeList'
 import { SchoppingList as ShoppingList } from './components/ShoppingList'
 import { AddRecipe } from './components/AddRecipe'
+import type { Recipe } from './types'
 
 function App() {
+  // const [refreshKey, setRefreshKey] = useState(0);
+
+  // const handleRecipeCreated = (recipe: Recipe) => {
+  //   console.log("recipe created: ", recipe);
+  //   setRefreshKey(prev => prev +1 );
+  // }
   
   return (
     <>
@@ -16,7 +24,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/RecipeList' element={<RecipeList />} />
         <Route path='/ShoppingList' element={<ShoppingList />} />
-        <Route path='/AddRecipe' element={<AddRecipe />} />
+        <Route path='/AddRecipe' element={<AddRecipe/>} />
       </Routes>
     </>
   )
--- recipeapp-ui/src/components/AddRecipe.tsx
+++ recipeapp-ui/src/components/AddRecipe.tsx
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import { type Recipe, type RecipeIngredientDetails } from "../types"
 import { recipeApi } from "../services/api";
 
-export const AddRecipe = (/*{onRecipeCreated}: Props*/) => {
+export const AddRecipe = () => {
 
     const [formData, setFormData] = useState<Recipe>({
         name: '',
-        timeToPrepare: 0
+        timeToPrepare: 0,
+        instructions: '',
+        servings: 0
     });
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
@@ -35,11 +37,44 @@ export const AddRecipe = (/*{onRecipeCreated}: Props*/) => {
 
     };
 
-
-
     return (
         <>
             <h1>Here you can add new recipes to your cookbook</h1>
+            <form onSubmit={handleSubmit}>
+                <h2>Add Recipe</h2>
+                {error && <div className="text-red-700">{error}</div>}
+
+                <input 
+                    type="text"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    placeholder="Name"
+                    required
+                />
+                <input 
+                    type="number"
+                    name="timeToPrepare"
+                    value={formData.timeToPrepare}
+                    placeholder="0"
+                    required
+                />
+                <input
+                    type="string"
+                    name="instructions"
+                    value={formData.instructions}
+                    placeholder="Instructions"
+                />
+                <input 
+                    type="number"
+                    name="servings"
+                    value={formData.servings}
+                    placeholder="0"
+                />
+                <button type="submit" disabled={loading}>
+                    {loading ? 'Creating...' : 'Add recipe'}
+                </button>
+            </form>
         </>
     );
 }
