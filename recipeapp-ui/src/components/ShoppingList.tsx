fe7bb16ec67930b79b49a0d8fa2b542071a4cdab
lehoucqgj <gertjan.lehoucq@gmail.com>
2025-12-25 14:34:18 +0100

    forgot about adding the same ingredients, so that they dont appear multiple times in the shopping list.

--- recipeapp-ui/src/components/ShoppingList.tsx
+++ recipeapp-ui/src/components/ShoppingList.tsx
@@ -5,6 +5,20 @@ interface ShoppingListProps {
 }
 
 export const SchoppingList = ({ items }: ShoppingListProps) => {
+    const groupIngredients = (items: RecipeIngredientDetails[]) => {
+        const grouped = items.reduce((acc, item) => {
+            if(!acc[item.name]){
+                acc[item.name] = {...item};
+            } else {
+                acc[item.name].quantity += item.quantity;
+            }
+            return acc;
+        }, {} as Record<string, RecipeIngredientDetails>);
+        return Object.values(grouped);
+    }
+
+    const groupedIngredients = groupIngredients(items);
+
     return (
         <>
             <h1>Your shopping list for this week</h1>
@@ -13,7 +27,7 @@ export const SchoppingList = ({ items }: ShoppingListProps) => {
                     <p>No items in your shoppinglist.</p>
                 ) : (
                     <ul>
-                        {items.map((item) => (
+                        {groupedIngredients.map((item) => (
                             <li key={`${item.recipeId}-${item.ingredientId}`}>
                                 {item.name}: {item.quantity} {item.quantifier}
                             </li>
