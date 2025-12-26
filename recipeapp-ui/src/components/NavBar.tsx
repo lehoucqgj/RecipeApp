d518d4cc85aa6f17571fe3b42cdb79429de35745
lehoucqgj <gertjan.lehoucq@gmail.com>
2025-12-26 22:59:25 +0100

    the horror! the horror! (me reflecting on the darkness of graphic design)

--- recipeapp-ui/src/components/NavBar.tsx
+++ recipeapp-ui/src/components/NavBar.tsx
@@ -1,5 +1,4 @@
 import { Link, useLocation } from "react-router-dom";
-import { Home } from "./Home";
 
 export const NavBar = () => {
     const location = useLocation();
@@ -12,13 +11,13 @@ export const NavBar = () => {
 
     return (
         <nav>
-            <div className="container flex items-center justify-center p-6 mx-auto text-[#E1CA96]">
+            <div className="container flex items-center justify-center p-6 mx-auto text-[#dbbc74]">
                 {links.map(link =>(
                     <p  key={link.to}
                         className={`border-b-2 mx-1.5 sm:mx-6 ${
                             location.pathname === link.to
                                 ? "text-[#dbbc74] border-[#9E0031]"
-                                : "border-transparent hover:text-[#e0cfa8] hover:border-[#9E0031]"
+                                : "border-transparent hover:text-[#e0ca96] hover:border-[#9E0031]"
                         }`}>
                         <Link to={link.to}>{link.label}</Link>
                     </p>
--- recipeapp-ui/src/components/RecipeComponent.tsx
+++ recipeapp-ui/src/components/RecipeComponent.tsx
@@ -1,10 +1,23 @@
 
 
 export const RecipeComponent = () => {
+    const pinkClass = "text-[#F0D3F7]";
     return (
         <>
-            <div className="m-2 p-1 w-1/2 border rounded border-blue-500">
-                <p className="pb-1 text-[#e0ccc8]">Everzwijnragout</p>
+            <div className="m-2 p-1 w-1/2 border rounded-xl border-[#9e0031] bg-[#1c2737]">
+                <p className="pb-1 font-bold text-[#dbbc74] border-b border-[#9e0031]">Everzwijnragout</p>
+                <p className={pinkClass}>uitleg</p>
+                <p className={pinkClass}>nog wa uitleg</p>
+                <div className="container flex  justify-center my-2">
+                    <ul className="flex-1 text-center text-[#F0D3F7]">
+                        <li>patat</li>
+                        <li>tonijn</li>
+                    </ul>
+                    <ul className="flex-1 text-center text-[#F0D3F7]">
+                        <li>5</li>
+                        <li>1 kg</li>
+                    </ul>
+                </div>
             </div>
         </>
     )
