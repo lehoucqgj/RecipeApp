38a3ab1e855b8ba71f43a633286ab613dedd0088
lehoucqgj <gertjan.lehoucq@gmail.com>
2025-12-26 23:31:07 +0100

    good enough

--- recipeapp-ui/src/components/RecipeComponent.tsx
+++ recipeapp-ui/src/components/RecipeComponent.tsx
@@ -4,11 +4,11 @@ export const RecipeComponent = () => {
     const pinkClass = "text-[#e3e5e5]";
     return (
         <>
-            <div className="m-2 p-1 w-1/2 border-2 rounded-xl border-[#1b4c48] bg-[#242b38]">
-                <p className="pb-1 font-bold text-[#e6f2f1] border-b-2 border-[#1b4c48]">Everzwijnragout</p>
+            <div className="m-2 p-1 w-1/2 border rounded-xl border-[#121918] bg-[#242b38]">
+                <p className="pb-1 font-bold text-fuchsia-800 border-b border-[#121918]">Everzwijnragout</p>
                 <p className={pinkClass}>uitleg</p>
                 <p className={pinkClass}>nog wa uitleg</p>
-                <div className="container flex  justify-center my-2 border-t-2 border-[#1b4c48]">
+                <div className="container flex  justify-center my-2 border-t border-[#121918]">
                     <ul className="flex-1 text-center text-[#e3e5e5]">
                         <li>patat</li>
                         <li>tonijn</li>
